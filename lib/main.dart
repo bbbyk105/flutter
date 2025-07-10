e86eb6c0596229f900d14e9f63a4cee0d79db2f5
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-07-10 12:51:28 +0900

    rowとcolumn

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const a = MaterialApp(
-    home: Scaffold(body: Center(child: Text("banana"))),
+  //カラム
+  final col = Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: const [Text("レモン"), Text("りんご"), Text("ブドウ")],
+  );
+
+  final row = Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [col, col, col],
+  );
+
+  // final col = Row(
+  //   mainAxisAlignment: MainAxisAlignment.center,
+  //   crossAxisAlignment: CrossAxisAlignment.center,
+  //   children: const [Text("レモン"), Text("りんご"), Text("ブドウ")],
+  // );
+
+  final a = MaterialApp(
+    home: Scaffold(body: Center(child: row)),
   );
   runApp(a);
 }
